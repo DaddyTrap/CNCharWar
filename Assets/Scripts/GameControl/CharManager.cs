f32c88ca0117dd0e121d89708626cfe6dd909809
DaddyTrap <914670787@qq.com>
2018-05-25 20:18:40 +0800

    :truck: :recycle: :fire: 删除误上传的DebugScene

---
+++ Assets/Scripts/GameControl/CharManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharManager : MonoBehaviour {
+
+	/* Singleton Begin */
+	private static CharManager instance_;
+
+	public static CharManager instance {
+		get {
+			return instance_;
+		}
+	}
+
+	void Awake() {
+		if (!instance_) {
+			instance_ = this;
+		} else {
+			this.enabled = false;
+		}
+	}
+	/* Singleton End */
+
+	void LoadJson() {
+
+	}
+}
