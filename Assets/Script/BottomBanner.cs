6f558c63eb7ef7af773e0a1c9f08f96d1c81f3a5
kotomineshiki <31608000+kotomineshiki@users.noreply.github.com>
2018-05-25 19:56:54 +0800

    增加目录material和script
    正在完善BottomBanner

---
+++ Assets/Script/BottomBanner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottomBanner : MonoBehaviour {
+    public GameObject template;//字的模板
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void AddWord(string word)
+    {
+        GameObject newWord = Instantiate(template);
+        newWord.name = word;
+
+    }
+}
