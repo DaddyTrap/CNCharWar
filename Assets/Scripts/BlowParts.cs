6e6d146125530f3233047268ad895714b8bd1c08
kotomineshiki <31608000+kotomineshiki@users.noreply.github.com>
2018-05-26 16:21:46 +0800

    吹飞部件完成（吹飞后效果待完成）

--- Assets/Scripts/BlowParts.cs
+++ Assets/Scripts/BlowParts.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlowParts : MonoBehaviour {
+public class BlowParts : MonoBehaviour {//需要配合collider（poly）
     public int currentPart;//
     public bool canBlow=false;
 	// Use this for initialization
@@ -16,9 +16,9 @@ public class BlowParts : MonoBehaviour {
         {
             this.transform.GetChild(currentPart).gameObject.AddComponent<Rigidbody2D>();//给该部件添加RigidBody2D
             this.transform.GetChild(currentPart).gameObject.GetComponent<Animator>().SetBool("isStop", true);//停止该部件的动画
-            this.transform.GetChild(currentPart).GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-10, 10), Random.Range(-3, 17));//随机发射一个方向
+            this.transform.GetChild(currentPart).GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-15, 15), Random.Range(0, 20));//随机发射一个方向
             this.transform.GetChild(currentPart).GetComponent<Rigidbody2D>().angularVelocity = Random.Range(-90, 90);
-
+            this.transform.GetChild(currentPart).GetComponent<Rigidbody2D>().drag = 0.3f;
             canBlow = false;
             currentPart++;
         }
